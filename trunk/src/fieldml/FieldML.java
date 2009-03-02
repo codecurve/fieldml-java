bac68a18d1e796d1eaca229aac67e86fd8c20970
caton.little@gmail.com <caton.little@gmail.com@c4eeaa98-0475-11de-ada4-5153b8187bf2>
2009-03-02 22:31:30 +0000

    Renamed FieldmlHandler to FieldmlSaxHandler. XML now allows importing domains.

--- trunk/src/fieldml/FieldML.java
+++ trunk/src/fieldml/FieldML.java
@@ -9,9 +9,9 @@ public class FieldML
     {
         Domain parent = Domain.get( parentId );
 
-        if( !( parent instanceof CompositeDomain ) )
+        if( ( parent == null ) || !( parent instanceof CompositeDomain ) )
         {
-
+            //ERROR
         }
 
         return new CompositeDomain( (CompositeDomain)parent, name ).getId();
@@ -20,6 +20,7 @@ public class FieldML
 
     public static int FieldML_CreateContinuousDomain( int parentId, double min, double max )
     {
+        //STUB Create an infinite, semi-infinite or finite continuous domain.
         return 0;
     }
 
@@ -28,11 +29,40 @@ public class FieldML
     {
         Domain parent = Domain.get( parentId );
 
-        if( !( parent instanceof CompositeDomain ) )
+        if( ( parent == null ) || !( parent instanceof CompositeDomain ) )
         {
-
+            //ERROR
         }
 
         return new DiscreteDomain( (CompositeDomain)parent, name, values, startIndex, count ).getId();
+    }
+
+
+    public static int FieldML_GetDomainId( String originalDomainName )
+    {
+        return Domain.getId( originalDomainName );
+    }
+
+
+    public static void FieldML_ImportDomain( Integer parentId, int originalDomainId, String newName )
+    {
+        Domain parent = Domain.get( parentId );
+
+        if( ( parent == null ) || !( parent instanceof CompositeDomain ) )
+        {
+            //ERROR
+
+        }
+        
+        CompositeDomain compositeParent = (CompositeDomain)parent;
+        
+        Domain child = Domain.get( originalDomainId );
+        
+        if( child == null )
+        {
+            //ERROR
+        }
+
+        child.importInto( compositeParent, newName );
     }
 }
--- trunk/src/fieldml/domain/Domain.java
+++ trunk/src/fieldml/domain/Domain.java
@@ -5,26 +5,44 @@ import java.util.*;
 public abstract class Domain
 {
     private static final int ID_FUDGE = 10000;
-    
+
     private static final char DOMAIN_DELIMITER = '.';
 
     private static final Map<Integer, Domain> domains;
 
+    private static final Map<String, Integer> domainIds;
+
     static
     {
         domains = new HashMap<Integer, Domain>();
+
+        domainIds = new HashMap<String, Integer>();
     }
-    
+
+
     public static Domain get( int id )
     {
         return domains.get( id );
     }
 
+
+    public static int getId( String name )
+    {
+        Integer id = domainIds.get( name );
+        
+        if( id == null )
+        {
+            return 0;
+        }
+        
+        return id;
+    }
+
     /**
-     * A globally unique integer identifying the domain, useful for internal (inter-process) and
-     * external (client-server) communication. In order to remain globally unique, this id number
-     * cannot be user-supplied. Domains can be imported from external sources, and can therefore have
-     * id numbers which are not known in advance by the user of the API when creating their own domains.
+     * A globally unique integer identifying the domain, useful for internal (inter-process) and external (client-server)
+     * communication. In order to remain globally unique, this id number cannot be user-supplied. Domains can be imported from
+     * external sources, and can therefore have id numbers which are not known in advance by the user of the API when creating
+     * their own domains.
      */
     private final int id;
 
@@ -38,17 +56,18 @@ public abstract class Domain
 
     public Domain( CompositeDomain parent, String name )
     {
+        this.name = name;
+
         id = domains.size() + ID_FUDGE;
 
         domains.put( id, this );
+        domainIds.put( getFullName(), id );
 
         this.parent = parent;
         if( parent != null )
         {
             parent.components.put( name, this );
         }
-        
-        this.name = name;
     }
 
 
@@ -74,6 +93,7 @@ public abstract class Domain
     {
         return id;
     }
-    
+
+
     public abstract void importInto( CompositeDomain parentDomain, String newName );
 }
